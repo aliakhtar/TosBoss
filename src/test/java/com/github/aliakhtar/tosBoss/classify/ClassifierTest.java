f19e7a2ab4b37a5d8fd24d94852ec7911ae810eb
Ali <ali.rac200@gmail.com>
2014-12-08 05:10:57 +0500

    Added train test for classifier.

---
+++ src/test/java/com/github/aliakhtar/tosBoss/classify/ClassifierTest.java
@@ -0,0 +1,13 @@
+package com.github.aliakhtar.tosBoss.classify;
+
+import org.junit.Test;
+
+public class ClassifierTest
+{
+
+    @Test
+    public void testTrain() throws Exception
+    {
+        Classifier.train();
+    }
+}
